8af12863bf6f1416bd8745fbfe6a44eb862ecae5
benwill777 <t.bennettwilliamsjr@gmail.com>
2018-06-04 11:12:32 -0400

    initialized user controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   namespace :api do
+    get 'users/index'
+    get 'users/show'
+  end
+  namespace :api do
     resources :users do
       resources :recipes 
     end
